aa4aea718f1730cbf99324638fdd9be67509e652
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-03 18:32:07 +0200

    factor out building of config list into a function

--- generator/src/main/kotlin/ConfigGenerator.kt
+++ generator/src/main/kotlin/ConfigGenerator.kt
@@ -4,50 +4,52 @@ import com.google.protobuf.ByteString
 import com.google.protobuf.ProtocolMessageEnum
 import java.io.File
 
-val configs: AppCompatConfigs = sortConfigs(
+fun getUnsortedConfigs(): List<AppCompatConfig> {
+    val l = mutableListOf<AppCompatConfig>()
 
-    app("com.google.android.GoogleCamera", certs(
+    l += app("com.google.android.GoogleCamera", certs(
         "f0fd6c5b410f25cb25c3b53346c8972fae30f8ee7411df910480ad6b2d60db83",
         "1975b2f17177bc89a5dff31f9e64a6cae281a53dc1d1d59b1d147fe1c82afa00",
     )) {
         minVersion = 65820000
         changes(DISABLE_HARDENED_MALLOC, DISABLE_MEMORY_TAGGING)
-    },
+    }
+
+    val mainGmsCerts = certs(
+        "7ce83c1b71f3d572fed04c8d40c5cb10ff75e6d87d9df6fbd53f0468c2905053", // "bd32"
+        "f0fd6c5b410f25cb25c3b53346c8972fae30f8ee7411df910480ad6b2d60db83", // "38d1"
+        "1975b2f17177bc89a5dff31f9e64a6cae281a53dc1d1d59b1d147fe1c82afa00", // "58e1"
+    )
 
     // Google Services Framework
-    app("com.google.android.gsf", mainGmsCerts()
-    ) {
+    l += app("com.google.android.gsf", mainGmsCerts) {
         minVersion = 34
-    },
+    }
 
     // GmsCore ("Play services")
-    app("com.google.android.gms", mainGmsCerts()
-    ) {
+    l += app("com.google.android.gms", mainGmsCerts) {
         minVersion = 23_40_00000
         changes(
             ALLOW_MEMORY_DYN_CODE_EXEC, // not clear why it's used
             ALLOW_STORAGE_DYN_CODE_EXEC, // for Dynamite modules
             SUPPRESS_NATIVE_DEBUGGING_NOTIFICATION, // doesn't break when ptrace access is blocked
         )
-    },
+    }
 
     // Play Store
-    app("com.android.vending", mainGmsCerts()
-    ) {
+    l += app("com.android.vending", mainGmsCerts) {
         minVersion = 8_38_1_0000
         changes(
             ALLOW_STORAGE_DYN_CODE_EXEC, // for GmsCore Dynamite modules
         )
-    },
-)
+    }
 
-fun mainGmsCerts() = certs(
-    "7ce83c1b71f3d572fed04c8d40c5cb10ff75e6d87d9df6fbd53f0468c2905053", // "bd32"
-    "f0fd6c5b410f25cb25c3b53346c8972fae30f8ee7411df910480ad6b2d60db83", // "38d1"
-    "1975b2f17177bc89a5dff31f9e64a6cae281a53dc1d1d59b1d147fe1c82afa00", // "58e1"
-)
+    return l
+}
 
 fun main() {
+    val configs: AppCompatConfigs = sortConfigs(getUnsortedConfigs())
+
     val f = File("../app_compat_configs.pb")
 
     f.outputStream().use {
@@ -57,7 +59,7 @@ fun main() {
     println("written configs to ${f.canonicalPath}")
 }
 
-fun sortConfigs(vararg list: AppCompatConfig) = AppCompatConfigs.newBuilder().run {
+fun sortConfigs(list: List<AppCompatConfig>) = AppCompatConfigs.newBuilder().run {
     addAllConfigs(list.sortedBy { it.packageSpec.pkgName })
     build()
 }
