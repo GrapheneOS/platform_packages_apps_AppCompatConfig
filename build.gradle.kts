7fbd84ec4303314cdb6cc7c0b5d658cf1466af4f
Daniel Micay <daniel.micay@grapheneos.org>
2024-03-01 13:01:40 -0500

    update Android Gradle plugin to 8.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import com.android.build.gradle.internal.utils.getBuildSrcPlugins
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("com.google.protobuf") version "0.9.4" apply false
 }
